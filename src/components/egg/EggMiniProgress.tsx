cf0b6b9ec6b252bb5abafc7403c440e4d41dcc82
seki-cpu <seki49126@gmail.com>
2026-09-08 13:33:23 +0900

    fix(ui): make Egg mini hydration-safe

--- src/components/egg/EggMiniProgress.tsx
+++ src/components/egg/EggMiniProgress.tsx
@@ -2,11 +2,24 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { getEggProgress, loadSaveData } from "../../lib/storage";
+import {
+  EGG_HATCH_XP,
+  getEggProgress,
+  loadSaveData,
+  type EggProgress,
+} from "../../lib/storage";
 import styles from "./eggMini.module.css";
 
 const progressEventName = "mydate-egg-progress-change";
 const rewardEventName = "mydate-egg-reward";
+
+const INITIAL_PROGRESS: EggProgress = {
+  totalXp: 0,
+  hatchXp: EGG_HATCH_XP,
+  progress: 0,
+  stage: "dormant",
+  hatched: false,
+};
 
 export function notifyEggProgressChanged() {
   window.dispatchEvent(new Event(progressEventName));
@@ -17,7 +30,7 @@ export function notifyEggRewardArrival() {
 }
 
 export function EggMiniProgress() {
-  const [progress, setProgress] = useState(() => getEggProgress(loadSaveData()));
+  const [progress, setProgress] = useState<EggProgress>(INITIAL_PROGRESS);
   const [bumping, setBumping] = useState(false);
 
   useEffect(() => {
